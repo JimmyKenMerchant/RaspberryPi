dae4abd6e71b252e9dc20145467ae3b402565d82
Kenta <k.i.drivingroad21@gmail.com>
2018-01-14 14:54:31 +0900

    Added Functions in math32.s

--- share/aloha_raspi/system32/library/math32.s
+++ share/aloha_raspi/system32/library/math32.s
@@ -580,6 +580,9 @@ math32_mat32_identity:
 	mov matrix, r0
 	pop {r0-r3}
 
+	cmp matrix, #0
+	beq math32_mat32_identity_common
+
 	mov one, #1
 	vmov vfp_one, one
 	vcvt.f32.s32 vfp_one, vfp_one
@@ -611,3 +614,368 @@ math32_mat32_identity:
 .unreq i
 .unreq matrix
 .unreq vfp_one
+
+
+/**
+ * function math32_mat32_multiply_vec
+ * Square Matrix and Column Vector Multiplication
+ * Caution! This Function Needs to Make VFPv2 Registers and Instructions Enable.
+ *
+ * Parameters
+ * r0: Matrix
+ * r1: Vector
+ * r2: Number of Vector Size
+ *
+ * Return: r0 (Value of Dot Product by Single Precision Float)
+ */
+.globl math32_mat32_multiply_vec
+math32_mat32_multiply_vec:
+	/* Auto (Local) Variables, but just Aliases */
+	matrix         .req r0 @ Parameter, Register for Argument and Result, Scratch Register
+	vector         .req r1 @ Parameter, Register for Argument, Scratch Register
+	number_vec     .req r2 @ Parameter, Register for Argument, Scratch Register
+	value1         .req r3
+	value2         .req r4
+	temp1          .req r5
+	temp2          .req r6
+	i              .req r7
+	offset         .req r8
+	vector_result  .req r9
+
+	/* VFP Registers */
+	vfp_value     .req d0
+	vfp_value1    .req s0
+	vfp_value2    .req s1
+	vfp_result    .req s2
+
+	push {r4-r9,lr}
+	vpush {s0-s3}
+
+	push {r0-r3}
+	mov r0, number_vec
+	bl heap32_malloc
+	mov vector_result, r0
+	pop {r0-r3}
+
+	cmp vector_result, #0
+	beq math32_mat32_multiply_vec_common
+
+	mov offset, #0
+	mov i, #0
+
+	math32_mat32_multiply_vec_row:
+		cmp i, number_vec
+		bge math32_mat32_multiply_vec_common
+
+		mov temp1, #0
+		vmov vfp_result, temp1
+		vcvt.f32.s32 vfp_result, vfp_result
+
+		mov temp1, number_vec
+		sub temp1, temp1, #1
+
+		add temp2, offset, temp1
+
+		math32_mat32_multiply_vec_row_column:
+			cmp temp1, #0
+			blt math32_mat32_multiply_vec_row_common
+
+			ldr value1, [matrix, temp2, lsl #2]         @ Substitution of Multiplication by 4
+			ldr value2, [vector, temp1, lsl #2]         @ Substitution of Multiplication by 4
+			vmov vfp_value, value1, value2
+			vmla.f32 vfp_result, vfp_value1, vfp_value2
+
+			sub temp1, temp1, #1
+			sub temp2, temp2, #1
+			b math32_mat32_multiply_vec_row_column
+
+		math32_mat32_multiply_vec_row_common:
+			vmov value1, vfp_result
+			str value1, [vector_result, i, lsl #2]      @ Substitution of Multiplication by 4
+			add offset, offset, number_vec
+			add i, i, #1
+
+			b math32_mat32_multiply_vec_row
+
+	math32_mat32_multiply_vec_common:
+		mov r0, vector_result
+		vpop {s0-s3}
+		pop {r4-r9,pc}
+
+.unreq matrix
+.unreq vector
+.unreq number_vec
+.unreq value1
+.unreq value2
+.unreq temp1
+.unreq temp2
+.unreq i
+.unreq offset
+.unreq vector_result
+.unreq vfp_value
+.unreq vfp_value1
+.unreq vfp_value2
+.unreq vfp_result
+
+
+/**
+ * function math32_vec32_normalize
+ * Normalize Vector
+ * Caution! This Function Needs to Make VFPv2 Registers and Instructions Enable.
+ * This Function Makes Allocated Memory Space from Heap.
+ *
+ * Parameters
+ * r1: Vector
+ * r0: Number of Vector Size
+ *
+ * Return: r0 (Vector to Have Been Normalized, If Zero Not Allocated Memory)
+ */
+.globl math32_vec32_normalize
+math32_vec32_normalize:
+	/* Auto (Local) Variables, but just Aliases */
+	vector        .req r0 @ Parameter, Register for Argument and Result, Scratch Register
+	number_vec    .req r1 @ Parameter, Register for Argument, Scratch Register
+	temp          .req r2
+	length        .req r3
+	vector_result .req r4
+	value         .req r5
+
+	/* VFP Registers */
+	vfp_length    .req s0
+	vfp_value     .req s1
+
+	push {r4-r5,lr}
+	vpush {s0-s1}
+
+	push {r0-r3}
+	mov r0, number_vec
+	bl heap32_malloc
+	mov vector_result, r0
+	pop {r0-r3}
+
+	cmp vector_result, #0
+	beq math32_vec32_normalize_common
+
+	mov temp, #0
+	vmov vfp_length, temp
+	vcvt.f32.s32 vfp_length, vfp_length
+
+	mov temp, number_vec
+	sub temp, temp, #1
+
+	math32_vec32_normalize_length:
+		cmp temp, #0
+		blt math32_vec32_normalize_checkzero
+
+		ldr value, [vector, temp, lsl #2]              @ Substitution of Multiplication by 4
+		vmov vfp_value, value
+		vmla.f32 vfp_length, vfp_value, vfp_value
+
+		sub temp, temp, #1
+		b math32_vec32_normalize_length
+
+	math32_vec32_normalize_checkzero:
+		vsqrt.f32 vfp_length, vfp_length
+		vcmp.f32 vfp_length, #0
+		vmrs apsr_nzcv, fpscr                          @ Transfer FPSCR Flags to CPSR's NZCV
+		beq math32_vec32_normalize_common
+
+		sub number_vec, number_vec, #1
+
+	math32_vec32_normalize_normal:
+		cmp number_vec, #0
+		blt math32_vec32_normalize_common
+
+		ldr value, [vector, number_vec, lsl #2]        @ Substitution of Multiplication by 4
+		vmov vfp_value, value
+		vdiv.f32 vfp_value, vfp_value, vfp_length
+		vmov value, vfp_value
+		str value, [vector_result, number_vec, lsl #2] @ Substitution of Multiplication by 4
+
+		b math32_vec32_normalize_normal
+
+	math32_vec32_normalize_common:
+		mov r0, vector_result
+		vpop {s0-s1}
+		pop {r4-r5,pc}
+
+.unreq vector
+.unreq number_vec
+.unreq temp
+.unreq length
+.unreq vector_result
+.unreq value
+.unreq vfp_length
+.unreq vfp_value
+
+
+/**
+ * function math32_vec32_dotproduct
+ * Dot Product
+ * Caution! This Function Needs to Make VFPv2 Registers and Instructions Enable.
+ *
+ * Parameters
+ * r0: Vector1
+ * r1: Vector2
+ * r2: Number of Vector Size
+ *
+ * Return: r0 (Value of Dot Product by Single Precision Float)
+ */
+.globl math32_vec32_dotproduct
+math32_vec32_dotproduct:
+	/* Auto (Local) Variables, but just Aliases */
+	vector1    .req r0 @ Parameter, Register for Argument and Result, Scratch Register
+	vector2    .req r1 @ Parameter, Register for Argument, Scratch Register
+	number_vec .req r2 @ Parameter, Register for Argument, Scratch Register
+	value      .req r3
+
+	/* VFP Registers */
+	vfp_dot    .req s0
+	vfp_value1 .req s1
+	vfp_value2 .req s2
+
+	vpush {s0-s2}
+
+	mov value, #0
+	vmov vfp_dot, value
+	vcvt.f32.s32 vfp_dot, vfp_dot
+
+	sub number_vec, number_vec, #1
+
+	math32_vec32_dotproduct_normal:
+		cmp number_vec, #0
+		blt math32_vec32_dotproduct_common
+
+		ldr value, [vector1, number_vec, lsl #2] @ Substitution of Multiplication by 4
+		vmov vfp_value1, value
+		ldr value, [vector2, number_vec, lsl #2] @ Substitution of Multiplication by 4
+		vmov vfp_value2, value
+		vmla.f32 vfp_dot, vfp_value1, vfp_value2
+
+		b math32_vec32_dotproduct_normal
+
+	math32_vec32_dotproduct_common:
+		vmov value, vfp_dot
+		mov r0, value
+		vpop {s0-s2}
+		mov pc, lr
+
+.unreq vector1
+.unreq vector2
+.unreq number_vec
+.unreq value
+.unreq vfp_dot
+.unreq vfp_value1
+.unreq vfp_value2
+
+
+/**
+ * function math32_vec32_crossprodut
+ * Cross Product
+ * Caution! This Function Needs to Make VFPv2 Registers and Instructions Enable.
+ * This Function Makes Allocated Memory Space from Heap.
+ *
+ * Parameters
+ * r1: Vector1, Must Be Three of Vector Size
+ * r1: Vector2, Must Be Three of Vector Size
+ *
+ * Return: r0 (Vector to Be Calculated, If Zero Not Allocated Memory)
+ */
+.globl math32_vec32_crossprodut
+math32_vec32_crossprodut:
+	/* Auto (Local) Variables, but just Aliases */
+	vector1       .req r0 @ Parameter, Register for Argument and Result, Scratch Register
+	vector2       .req r1 @ Parameter, Register for Argument, Scratch Register
+	number_vec    .req r2 @ Parameter, Register for Argument, Scratch Register
+	value1        .req r3
+	value2        .req r4
+	vector_result .req r5
+
+	/* VFP Registers */
+	vfp_value    .req d0
+	vfp_value1   .req s0
+	vfp_value2   .req s1
+	vfp_inter1   .req s2
+	vfp_inter2   .req s3
+	vfp_result   .req s4
+
+	push {r4-r5,lr}
+	vpush {s0-s4}
+
+	mov number_vec, #3
+
+	push {r0-r3}
+	mov r0, number_vec
+	bl heap32_malloc
+	mov vector_result, r0
+	pop {r0-r3}
+
+	/* X */
+
+	ldr value1, [vector1, #4]                   @ Vector1[1], Y
+	ldr value2, [vector2, #8]                   @ Vector2[2], Z
+	vmov vfp_value, value1, value2
+	vmul.f32 vfp_inter1, vfp_value1, vfp_value2
+
+	ldr value1, [vector1, #8]                   @ Vector1[2], Z
+	ldr value2, [vector2, #4]                   @ Vector2[1], Y
+	vmov vfp_value, value1, value2
+	vmul.f32 vfp_inter2, vfp_value1, vfp_value2
+
+	vsub.f32 vfp_result, vfp_inter1, vfp_inter2
+
+	vmov value1, vfp_result
+	str value1, [vector_result]                 @ Vector_result[0], X
+
+	/* Y */
+
+	ldr value1, [vector1, #8]                   @ Vector1[2], Z
+	ldr value2, [vector2]                       @ Vector2[0], X
+	vmov vfp_value, value1, value2
+	vmul.f32 vfp_inter1, vfp_value1, vfp_value2
+
+	ldr value1, [vector1]                       @ Vector1[0], X
+	ldr value2, [vector2, #8]                   @ Vector2[2], Z
+	vmov vfp_value, value1, value2
+	vmul.f32 vfp_inter2, vfp_value1, vfp_value2
+
+	vsub.f32 vfp_result, vfp_inter1, vfp_inter2
+
+	vmov value1, vfp_result
+	str value1, [vector_result, #4]             @ Vector_result[1], Y
+
+	/* Z */
+
+	ldr value1, [vector1]                       @ Vector1[0], X
+	ldr value2, [vector2, #4]                   @ Vector2[1], Y
+	vmov vfp_value, value1, value2
+	vmul.f32 vfp_inter1, vfp_value1, vfp_value2
+
+	ldr value1, [vector1, #4]                   @ Vector1[1], Y
+	ldr value2, [vector2]                       @ Vector2[0], X
+	vmov vfp_value, value1, value2
+	vmul.f32 vfp_inter2, vfp_value1, vfp_value2
+
+	vsub.f32 vfp_result, vfp_inter1, vfp_inter2
+
+	vmov value1, vfp_result
+	str value1, [vector_result, #8]             @ Vector_result[2], Z
+
+	math32_vec32_crossprodut_common:
+		mov r0, vector_result
+		vpop {s0-s4}
+		pop {r4-r5,pc}
+
+.unreq vector1
+.unreq vector2
+.unreq number_vec
+.unreq value1
+.unreq value2
+.unreq vector_result
+.unreq vfp_value
+.unreq vfp_value1
+.unreq vfp_value2
+.unreq vfp_inter1
+.unreq vfp_inter2
+.unreq vfp_result
+
