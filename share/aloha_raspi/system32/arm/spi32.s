a6dd8173b6b6ff4d48a17db80d3b38f252418343
Kenta <k.i.drivingroad21@gmail.com>
2018-08-13 13:13:11 +0900

    Modified Functions in spi32.s | Modified SPI Test

--- share/aloha_raspi/system32/arm/spi32.s
+++ share/aloha_raspi/system32/arm/spi32.s
@@ -145,51 +145,61 @@ spi32_spidone:
 
 /**
  * function spi32_spitx
- * SPI Transfer
+ * SPI Transfer with Little Endian (Most Significant Byte is Sent First)
  *
  * Parameters
  * r0: Data to Be Transferred
  * r1: Length of Transferred Data (Bytes, Up to 4)
  *
- * Return: r0 (0 as Success)
+ * Return: r0 (Number of Data To Be Transferred)
  */
 .globl spi32_spitx
 spi32_spitx:
 	/* Auto (Local) Variables, but just Aliases */
 	data_send  .req r0
-	i          .req r1
-	addr_spi   .req r2
-	byte       .req r3
-	temp       .req r4
+	length     .req r1
+	i          .req r2
+	addr_spi   .req r3
+	byte       .req r4
+	temp       .req r5
 
-	push {r4}
+	push {r4-r5}
 
 	mov addr_spi, #equ32_peripherals_base
 	add addr_spi, addr_spi, #equ32_spi0_base_upper
 	add addr_spi, addr_spi, #equ32_spi0_base_lower
 
-	cmp i, #4
-	movgt i, #4
-	sub i, i, #1
+	rev data_send, data_send                      @ Little Endian to Big Endian
+	mov i, #0
+
+	cmp length, #4
+	movhi length, #4
 
 	spi32_spitx_txfifo:
+		cmp i, length
+		bhs spi32_spitx_success
+
 		ldr temp, [addr_spi, #equ32_spi0_cs]
 		tst temp, #equ32_spi0_cs_txd              @ If Having Space on TxFIFO
+
 		lslne temp, i, #3                         @ Substitute of Multiplication by 8 (Per Byte)
 		lsrne byte, data_send, temp
 		strneb byte, [addr_spi, #equ32_spi0_fifo]
-		subne i, i, #1
-		cmp i, #0
-		bge spi32_spitx_txfifo
+		addne i, i, #1
+
+		macro32_dsb ip
+
+		b spi32_spitx_txfifo
 
 	spi32_spitx_success:
-		mov r0, #0
+		mov r0, i
 
 	spi32_spitx_common:
-		pop {r4}
+		pop {r4-r5}
 		mov pc, lr
 
 .unreq data_send
+.unreq length
 .unreq i
 .unreq addr_spi
 .unreq byte
@@ -198,58 +208,63 @@ spi32_spitx:
 
 /**
  * function spi32_spirx
- * SPI Receive
+ * SPI Receive with Little Endian (Most Significant Byte is Stored First in Bit[31:24])
  *
  * Parameters
- * r0: Length of Data Received (Bytes, Up to 4)
+ * r0: Length of Data to Be Received (Bytes, Up to 4)
  *
  * Return: r0 (Data to Be Received)
  */
 .globl spi32_spirx
 spi32_spirx:
 	/* Auto (Local) Variables, but just Aliases */
-	i            .req r0
+	length       .req r0
 	data_receive .req r1
-	addr_spi     .req r2
-	byte         .req r3
-	temp         .req r4
+	i            .req r2
+	addr_spi     .req r3
+	byte         .req r4
+	temp         .req r5
 
-	push {r4}
+	push {r4-r5}
 
 	mov addr_spi, #equ32_peripherals_base
 	add addr_spi, addr_spi, #equ32_spi0_base_upper
 	add addr_spi, addr_spi, #equ32_spi0_base_lower
 
 	mov data_receive, #0
+	mov i, #0
 
-	cmp i, #4
-	movgt i, #4
-	sub i, i, #1
+	cmp length, #4
+	movhi length, #4
 
 	spi32_spirx_rxfifo:
-		ldr temp, [addr_spi, #equ32_spi0_cs]
+		cmp i, length
+		bhs spi32_spirx_success
 
+		ldr temp, [addr_spi, #equ32_spi0_cs]
 		tst temp, #equ32_spi0_cs_rxd
 		beq spi32_spirx_success                 @ If Having No Data on RxFIFO
 
 		ldrb byte, [addr_spi, #equ32_spi0_fifo] @ If Having Data on RxFIFO
 		lsl temp, i, #3                         @ Substitute of Multiplication by 8 (Per Byte)
 		lsl byte, byte, temp
-		add data_receive, data_receive, byte
-		sub i, i, #1
-		cmp i, #0
+		orr data_receive, data_receive, byte
+		add i, i, #1
 
-		bge spi32_spirx_rxfifo
+		macro32_dsb ip
+
+		b spi32_spirx_rxfifo
 
 	spi32_spirx_success:
-		mov r0, data_receive
+		rev r0, data_receive                    @ Big Endian to Little Endian
 
 	spi32_spirx_common:
-		pop {r4}
+		pop {r4-r5}
 		mov pc, lr
 
-.unreq i
+.unreq length
 .unreq data_receive
+.unreq i
 .unreq addr_spi
 .unreq byte
 .unreq temp
@@ -257,7 +272,7 @@ spi32_spirx:
 
 /**
  * function spi32_spitx_memory
- * SPI Transfer with Memory
+ * SPI Transfer with Memory, Big Endian (Most Significant Byte on Offset Zero is Sent First)
  *
  * Parameters
  * r0: Pointer of Data to Be Transferred
@@ -285,18 +300,19 @@ spi32_spitx_memory:
 	mov i, #0
 
 	spi32_spitx_memory_txfifo:
+		cmp i, length
+		bhs spi32_spitx_memory_common
+
 		ldr temp, [addr_spi, #equ32_spi0_cs]
 		tst temp, #equ32_spi0_cs_txd
-		beq spi32_spitx_memory_common
 
-		ldrb byte, [data_send, i]
-		macro32_dsb ip
-		strb byte, [addr_spi, #equ32_spi0_fifo]
+		ldrneb byte, [data_send, i]
+		strneb byte, [addr_spi, #equ32_spi0_fifo]
+		addne i, i, #1
+
 		macro32_dsb ip
 
-		add i, i, #1
-		subs length, length, #1
-		bgt spi32_spitx_memory_txfifo
+		b spi32_spitx_memory_txfifo
 
 	spi32_spitx_memory_common:
 		mov r0, i
@@ -312,8 +328,8 @@ spi32_spitx_memory:
 
 
 /**
- * function  spi32_spirx_memory
- * SPI Receive with Memory
+ * function spi32_spirx_memory
+ * SPI Receive with Memory, Big Endian (Most Significant Byte is Stored First in Offset Zero)
  *
  * Parameters
  * r0: Pointer of Data to Be Received
@@ -322,8 +338,8 @@ spi32_spitx_memory:
  * Return: r0 (0 as Success, 1 and More as Error)
  * Error: Number of Bytes to Be Received
  */
-.globl  spi32_spirx_memory
- spi32_spirx_memory:
+.globl spi32_spirx_memory
+spi32_spirx_memory:
 	/* Auto (Local) Variables, but just Aliases */
 	data_receive .req r0
 	length       .req r1
@@ -340,21 +356,23 @@ spi32_spitx_memory:
 
 	mov i, #0
 
-	 spi32_spirx_memory_rxfifo:
+	spi32_spirx_memory_rxfifo:
+		cmp i, length
+		bhs spi32_spirx_memory_common
+
 		ldr temp, [addr_spi, #equ32_spi0_cs]
 		tst temp, #equ32_spi0_cs_rxd
 		beq spi32_spirx_memory_common
 
 		ldrb byte, [addr_spi, #equ32_spi0_fifo]
-		macro32_dsb ip
 		strb byte, [data_receive, i]
+		add i, i, #1
+
 		macro32_dsb ip
 
-		add i, i, #1
-		subs length, length, #1
-		bgt  spi32_spirx_memory_rxfifo
+		b spi32_spirx_memory_rxfifo
 
-	 spi32_spirx_memory_common:
+	spi32_spirx_memory_common:
 		mov r0, i
 		pop {r4-r5}
 		mov pc, lr
