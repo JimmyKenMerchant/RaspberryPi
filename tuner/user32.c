d5681f9b89c2eef051d21506f86f6a490e017471
Kenta <k.i.drivingroad21@gmail.com>
2019-02-13 14:50:53 +0900

    Making Tuner, Sampling

--- tuner/user32.c
+++ tuner/user32.c
@@ -20,11 +20,14 @@ int32 _user_start()
 	while(True) {
 		if ( TUNER_FIQ_FLAG_BACK == flag_flip ) {
 			flag_flip = flag_flip ^ true;
-print32_debug(flag_flip, 100, 100);
+//print32_debug(flag_flip, 100, 100);
+print32_debug_hexa(TUNER_FIQ_BUFFER, 0, 0, 256);
+
+print32_debug_hexa(TUNER_FIQ_BUFFER + 16320 * 4, 0, 300, 260);
 		}
-print32_debug(TUNER_FIQ_FLAG_BACK, 100, 112);
-print32_debug(TUNER_FIQ_BUFFER, 100, 124);
-		
+		arm32_dsb();
+//print32_debug(TUNER_FIQ_FLAG_BACK, 100, 112);
+//print32_debug(TUNER_FIQ_BUFFER, 100, 124);
 	}
 
 	return EXIT_SUCCESS;
