eb0b70b9ca114c5aa7b5f68911ad82c1987ef9cc
Kenta Ishii <k.i.drivingroad21@gmail.com>
2019-10-23 11:31:39 +0900

    Modified V3D Test, Multi-cube

--- v3d_test/user32.c
+++ v3d_test/user32.c
@@ -35,10 +35,11 @@ typedef struct user32_Legend3D {
 	float32 scale[3];
 	float32 versor_angle;
 	float32 versor_vector[3];
-} Legend3D;
+} _Legend3D;
 
 /* Declare Unique Functions */
-void va_set_triangle3d( _GPUMemory* vertex_array, float32* vertices, uint32 num_vertex, obj matrix, uint32 width_pixel, uint32 height_pixel );
+void va_set_triangle3d( _GPUMemory* vertex_array, float32* vertices, uint32 num_vertex, _Legend3D* legend3d, obj mat_p_v, uint32 width_pixel, uint32 height_pixel );
+void legend3D_change_position( _Legend3D* legend3d );
 
 /* Declare Unique Global Variables, Zero Can't Be Stored If You Want to Define with Declaration */
 uint32 va_count; // Global for Setting Vertex Array
@@ -156,7 +157,8 @@ int32 _user_start()
 	_Texture2D *texture2d_background;
 	_Texture2D *texture2d_1;
 	_Texture2D *texture2d_2;
-	Legend3D **cubes = (Legend3D**)heap32_malloc( 2 );
+	_Legend3D *background;
+	_Legend3D **cubes = (_Legend3D**)heap32_malloc( 2 );
 	uint32 cubes_length = 0;
 	uint32 width_pixel = FB32_WIDTH;
 	uint32 height_pixel = FB32_HEIGHT;
@@ -169,16 +171,9 @@ int32 _user_start()
 	obj mat_view = mtx32_view3d( (obj)camera_position, (obj)camera_target, (obj)camera_up );
 	obj mat_projection = mtx32_perspective3d( 75.0f, 1.3333333f, 0.2f, 4.0f );
 	obj mat_p_v = mtx32_multiply( mat_view, mat_projection, 4 ); // Projection, View
-	obj mat_translate;
-	obj mat_scale;
-	obj mat_t_s; // Translate and Scale
-	obj versor;
-	obj mat_versor;
-	obj mat_model;
-	obj mat_p_v_m; // Projection, View, Model
 
 	vertex_array = (_GPUMemory*)heap32_malloc( _wordsizeof( _GPUMemory ) );
-	_gpumemory_init( vertex_array, 1008, 16, 0xC );
+	_gpumemory_init( vertex_array, 1872, 16, 0xC );
 
 	additional_uniforms = (_GPUMemory*)heap32_malloc( _wordsizeof( _GPUMemory ) );
 	_gpumemory_init( additional_uniforms, 256, 16, 0xC );
@@ -210,7 +205,22 @@ int32 _user_start()
 	bit32_convert_endianness( texture2d_2->gpu&0x3FFFFFFF, DATA_COLOR32_SAMPLE_IMAGE2_SIZE, 4 );
 	draw32_rgba_to_argb( texture2d_2->gpu&0x3FFFFFFF, DATA_COLOR32_SAMPLE_IMAGE2_SIZE );
 
-	cubes[0] = (Legend3D*)heap32_malloc( _wordsizeof( Legend3D ) );
+	background = (_Legend3D*)heap32_malloc( _wordsizeof( _Legend3D ) );
+	background->position[0] = 0.0f; // X
+	background->position[1] = 0.0f; // Y
+	background->position[2] = 0.0f; // Z
+	background->speed_position[0] = 0.0f; // X
+	background->speed_position[1] = 0.0f; // Y
+	background->speed_position[2] = 0.0f; // Z
+	background->scale[0] = 1.0f; // X
+	background->scale[1] = 1.0f; // Y
+	background->scale[2] = 1.0f; // Z
+	background->versor_angle = 0.0f;
+	background->versor_vector[0] = 0.0f; // X
+	background->versor_vector[1] = 0.0f; // Y
+	background->versor_vector[2] = 0.0f; // Z
+
+	cubes[0] = (_Legend3D*)heap32_malloc( _wordsizeof( _Legend3D ) );
 	cubes_length++;
 	cubes[0]->position[0] = -0.5f; // X
 	cubes[0]->position[1] = 0.0f; // Y
@@ -226,13 +236,26 @@ int32 _user_start()
 	cubes[0]->versor_vector[1] = 1.0f; // Y
 	cubes[0]->versor_vector[2] = 1.0f; // Z
 
+	cubes[1] = (_Legend3D*)heap32_malloc( _wordsizeof( _Legend3D ) );
+	cubes_length++;
+	cubes[1]->position[0] = -0.05f; // X
+	cubes[1]->position[1] = 0.35f; // Y
+	cubes[1]->position[2] = -0.2f; // Z
+	cubes[1]->speed_position[0] = 0.005f; // X
+	cubes[1]->speed_position[1] = 0.0f; // Y
+	cubes[1]->speed_position[2] = 0.02f; // Z
+	cubes[1]->scale[0] = 0.95f; // X
+	cubes[1]->scale[1] = 0.95f; // Y
+	cubes[1]->scale[2] = 0.95f; // Z
+	cubes[1]->versor_angle = 0.0f;
+	cubes[1]->versor_vector[0] = 1.0f; // X
+	cubes[1]->versor_vector[1] = 1.0f; // Y
+	cubes[1]->versor_vector[2] = 1.0f; // Z
+
 	// Set Textures to Array
 	additional_uniforms->arm[0].u32 = texture2d_background->gpu;
 	additional_uniforms->arm[1].u32 = texture2d_1->gpu;
 	additional_uniforms->arm[2].u32 = texture2d_2->gpu;
-
-	// Set Background Vertices
-	va_set_triangle3d( vertex_array, background_vertices, 6, mat_identity4, width_pixel, height_pixel );
 
 	// Make Control Lists for Binning and Rendering
 	objectv3d = (_ObjectV3D*)heap32_malloc( _wordsizeof( _ObjectV3D ) );
@@ -246,6 +269,7 @@ int32 _user_start()
 	_control_qpul2cache( 0b101 );
 	_clear_qpucache( 0x0F0F0F0F );
 
+	va_count = 0; // Reset
 	while(True) {
 		if ( OS_FIQ_ONEFRAME ) {
 			/**
@@ -253,24 +277,6 @@ int32 _user_start()
 			 */
 			if ( ++count_update >= MAXCOUNT_UPDATE ) { // Increment Count Before, Then Compare with Number
 _stopwatch_start();
-
-				// Make Matrix
-				mat_translate = mtx32_translate3d( (obj)cubes[0]->position );
-				mat_scale = mtx32_scale3d( (obj)cubes[0]->scale );
-				mat_t_s = mtx32_multiply( mat_scale, mat_translate, 4 );
-				versor = mtx32_versor( cubes[0]->versor_angle, (obj)cubes[0]->versor_vector );
-				mat_versor = mtx32_versortomatrix( versor );
-				mat_model = mtx32_multiply( mat_t_s, mat_versor, 4 );
-				mat_p_v_m = mtx32_multiply( mat_p_v, mat_model, 4 );
-				va_count = 6; // Offset for Background Vertices
-				va_set_triangle3d( vertex_array, cube_vertices, 36, mat_p_v_m, width_pixel, height_pixel );
-				heap32_mfree( mat_translate );
-				heap32_mfree( mat_scale );
-				heap32_mfree( mat_t_s );
-				heap32_mfree( versor );
-				heap32_mfree( mat_versor );
-				heap32_mfree( mat_model );
-				heap32_mfree( mat_p_v_m );
 
 				// Actual Rendering
 				_clear_cl_rendering( COLOR32_CYAN, 0xFFFFFF, 0x0, 0x0 );
@@ -280,29 +286,18 @@ _stopwatch_start();
 				_execute_cl_binning( 4, va_count, 0, 0xFF0000 ); // TRIANGLE, 42 Vertices, Index from 0
 				_execute_cl_rendering( 0xFF0000 ); // The Point to Actually Draw Using Vertices
 
-				// Angle Change
-				cubes[0]->versor_angle = vfp32_fadd( cubes[0]->versor_angle, 0.5f );
-				if( vfp32_fge( cubes[0]->versor_angle, 360.0f ) ) cubes[0]->versor_angle = 0.0f;
+				// Calculate Vertices, Z Sort is Needed for Proper Drawing
+				va_count = 0; // Reset
+				va_set_triangle3d( vertex_array, background_vertices, 6, background, mat_identity4, width_pixel, height_pixel );
+				va_set_triangle3d( vertex_array, cube_vertices, 36, cubes[1], mat_p_v, width_pixel, height_pixel );
+				va_set_triangle3d( vertex_array, cube_vertices, 36, cubes[0], mat_p_v, width_pixel, height_pixel );
 
-				// Position Change
-				cubes[0]->position[0] = vfp32_fadd( cubes[0]->position[0], cubes[0]->speed_position[0] ); // X
-				if( vfp32_fge( cubes[0]->position[0], 0.5f ) ) cubes[0]->position[0] = -0.5f;
-				cubes[0]->position[2] = vfp32_fadd( cubes[0]->position[2], cubes[0]->speed_position[2] ); // Y
-				if( vfp32_fge( cubes[0]->position[2], 2.0f ) ) cubes[0]->position[2] = -2.0f;
-
-				// Scale Change
-				// For accuracy, calculate the distance between eye and object, but not this linear incrementation.
-				cubes[0]->scale[0] = vfp32_fadd( cubes[0]->scale[0], 0.005f );
-				if( vfp32_fge( cubes[0]->scale[0], 1.5f ) ) cubes[0]->scale[0] = 0.5f;
-				cubes[0]->scale[1] = vfp32_fadd( cubes[0]->scale[1], 0.005f );
-				if( vfp32_fge( cubes[0]->scale[1], 1.5f ) ) cubes[0]->scale[1] = 0.5f;
-				cubes[0]->scale[2] = vfp32_fadd( cubes[0]->scale[2], 0.005f );
-				if( vfp32_fge( cubes[0]->scale[2], 1.5f ) ) cubes[0]->scale[2] = 0.5f;
+				legend3D_change_position( cubes[0] );
+				legend3D_change_position( cubes[1] );
 
 uint32 time = _stopwatch_end();
 String num_string = cvt32_int32_to_string_deci( time, 0, 0 );
 print32_string( num_string, 0, 0, str32_strlen( num_string ) );
-print32_debug( mat_versor, 0, 50 );
 //print32_debug_hexa( FB32_FRAMEBUFFER->addr + ((800 * 324) + 400)*4, 0, 64, 256 );
 heap32_mfree( (obj)num_string );
 
@@ -325,7 +320,9 @@ heap32_mfree( (obj)num_string );
 	heap32_mfree( (obj)overspillmemory );
 	_fragmentshader_free( fragmentshader );
 	heap32_mfree( (obj)fragmentshader );
+	heap32_mfree( (obj)background );
 	heap32_mfree( (obj)cubes[0] );
+	heap32_mfree( (obj)cubes[1] );
 	heap32_mfree( (obj)cubes );
 	_texture2d_free( texture2d_0 );
 	heap32_mfree( (obj)texture2d_0 );
@@ -343,13 +340,20 @@ heap32_mfree( (obj)num_string );
 	return EXIT_SUCCESS;
 }
 
-void va_set_triangle3d( _GPUMemory* vertex_array, float32* vertices, uint32 num_vertex, obj matrix, uint32 width_pixel, uint32 height_pixel ) {
+void va_set_triangle3d( _GPUMemory* vertex_array, float32* vertices, uint32 num_vertex, _Legend3D* legend3d, obj mat_p_v, uint32 width_pixel, uint32 height_pixel ) {
 	float32 *vector_xyzw = (float32*)heap32_malloc( 4 );
 	float32 *result;
 	int16 int_x;
 	int16 int_y;
 	float32 width_float = vfp32_u32tof32( width_pixel );
 	float32 height_float = vfp32_u32tof32( height_pixel );
+	obj mat_translate = mtx32_translate3d( (obj)legend3d->position );
+	obj mat_scale = mtx32_scale3d( (obj)legend3d->scale );
+	obj mat_t_s = mtx32_multiply( mat_scale, mat_translate, 4 ); // Translate and Scale
+	obj versor = mtx32_versor( legend3d->versor_angle, (obj)legend3d->versor_vector );
+	obj mat_versor = mtx32_versortomatrix( versor );
+	obj mat_model = mtx32_multiply( mat_t_s, mat_versor, 4 );
+	obj mat_p_v_m = mtx32_multiply( mat_p_v, mat_model, 4 ); // Projection, View, Model
 
 	for ( uint32 i = 0; i < num_vertex; i++ ) {
 
@@ -365,7 +369,7 @@ void va_set_triangle3d( _GPUMemory* vertex_array, float32* vertices, uint32 num_
 		/* W of Vector */
 		vector_xyzw[3] = 1.0f;
 
-		result = (float32*)mtx32_multiply_vec( matrix, (obj)vector_xyzw, 4 );
+		result = (float32*)mtx32_multiply_vec( mat_p_v_m, (obj)vector_xyzw, 4 );
 
 		/* Software (-1.0f, 1.0f) Coordinate to Hardware (0.0f, 1.0f) Coordinate, Flip Y and Z Coordinate */
 		result[0] = vfp32_fadd( result[0], 1.0f );
@@ -399,6 +403,36 @@ void va_set_triangle3d( _GPUMemory* vertex_array, float32* vertices, uint32 num_
 		arm32_dsb();
 	}
 
+print32_debug( mat_versor, 0, 50 );
+
 	heap32_mfree( (obj)vector_xyzw );
+	heap32_mfree( mat_translate );
+	heap32_mfree( mat_scale );
+	heap32_mfree( mat_t_s );
+	heap32_mfree( versor );
+	heap32_mfree( mat_versor );
+	heap32_mfree( mat_model );
+	heap32_mfree( mat_p_v_m );
+}
+
+void legend3D_change_position( _Legend3D* legend3d ) {
+	// Angle Change
+	legend3d->versor_angle = vfp32_fadd( legend3d->versor_angle, 0.5f );
+	if( vfp32_fge( legend3d->versor_angle, 360.0f ) ) legend3d->versor_angle = 0.0f;
+
+	// Position Change
+	legend3d->position[0] = vfp32_fadd( legend3d->position[0], legend3d->speed_position[0] ); // X
+	if( vfp32_fge( legend3d->position[0], 0.5f ) ) legend3d->position[0] = -0.5f;
+	legend3d->position[2] = vfp32_fadd( legend3d->position[2], legend3d->speed_position[2] ); // Z
+	if( vfp32_fge( legend3d->position[2], 2.0f ) ) legend3d->position[2] = -2.0f;
+
+	// Scale Change
+	// For accuracy, calculate the distance between eye and object, but not this linear incrementation.
+	legend3d->scale[0] = vfp32_fadd( legend3d->scale[0], 0.005f );
+	if( vfp32_fge( legend3d->scale[0], 1.5f ) ) legend3d->scale[0] = 0.5f;
+	legend3d->scale[1] = vfp32_fadd( legend3d->scale[1], 0.005f );
+	if( vfp32_fge( legend3d->scale[1], 1.5f ) ) legend3d->scale[1] = 0.5f;
+	legend3d->scale[2] = vfp32_fadd( legend3d->scale[2], 0.005f );
+	if( vfp32_fge( legend3d->scale[2], 1.5f ) ) legend3d->scale[2] = 0.5f;
 }
 
