02858d4421e0183f5a164d6595d65506a842d6f7
Kenta Ishii <k.i.drivingroad21@gmail.com>
2020-02-22 14:12:06 +0900

    Modified DMX512 Transmitter Test 1. ARM Timer to Transmission from GPIO Clock 2. Removed IRQ with GPIO 3. Naming of Variables

--- dmx512_tx_test/user32.c
+++ dmx512_tx_test/user32.c
@@ -24,29 +24,25 @@
 #define GPIO_EOP_TOGGLE                 16 // End of Packet
 #define DMX512_LENGTH                   512
 
-extern uint32 OS_IRQ_COUNT;
-extern uint32 OS_IRQ_TRANSMIT;
-extern uint32 OS_IRQ_SWAP;
 extern uint32 OS_FIQ_COUNT;
-extern uint32 OS_FIQ_RXFIFO;
-extern uint32 OS_FIQ_TXFIFO;
-extern uint32 OS_FIQ_BREAK;
+extern uint32 OS_FIQ_TRANSMIT;
+extern uint32 OS_FIQ_SWAP;
 
 uint32 count_receive;
 uint32 count_error;
 uint32 increment;
 uchar8 turn;
-uchar8 number_command;
-uint16 target_address;
+uchar8 data_mode;
+uint16 index_slot;
 uint16 buffer_data;
-uchar8 target_address_position; // 0 = Bit[3:0], 1 = Bit[7:0], 2 = Bit[8]
-bool buffer_data_upper;
+uchar8 index_slot_position; // 0 = Bit[3:0], 1 = Bit[7:0], 2 = Bit[8]
+bool buffer_data_upper_flag;
 
 int32 _user_start() {
 	/* Local Variables */
 	uint32 detect_parallel = 0;
-	obj front_address_swap;
-	obj back_address_swap;
+	obj front_buffer_swap;
+	obj back_buffer_swap;
 
 	/* Initialization of Global Variables */
 	count_receive = 0;
@@ -54,11 +50,11 @@ int32 _user_start() {
 	//uint32 error;
 	increment = 0;
 	turn = 0;
-	number_command = 0;
-	target_address = 0;
+	data_mode = 0;
+	index_slot = 0;
 	buffer_data = 0;
-	target_address_position = 0;
-	buffer_data_upper = false;
+	index_slot_position = 0;
+	buffer_data_upper_flag = false;
 
 	//print32_debug( DMX32_BUFFER_FRONT, 100, 100 );
 	//print32_debug( DMX32_BUFFER_BACK, 100, 112 );
@@ -70,22 +66,21 @@ int32 _user_start() {
 	while( True ) {
 		/* DMX512 */
 		/*
-		print32_debug( OS_IRQ_COUNT, 100, 52 );
-		print32_debug( OS_FIQ_COUNT, 100, 64 );
-		if ( OS_IRQ_TRANSMIT ) {
+		print32_debug( OS_FIQ_COUNT, 100, 52 );
+		if ( OS_FIQ_TRANSMIT ) {
 			_store_8( DMX32_BUFFER_BACK + increment, turn );
 			increment++;
 			if ( increment > 512 ) {
 				increment = 0;
 				turn++;
 			}
-			_store_32( (uint32)&OS_IRQ_TRANSMIT, 0x00 );
+			_store_32( (uint32)&OS_FIQ_TRANSMIT, 0x00 );
 		}
 		*/
 
-		if ( OS_IRQ_TRANSMIT ) {
+		if ( OS_FIQ_TRANSMIT ) {
 			_gpiotoggle( GPIO_EOP_TOGGLE, _GPIOTOGGLE_SWAP );
-			_store_32( (uint32)&OS_IRQ_TRANSMIT, 0x00 );
+			_store_32( (uint32)&OS_FIQ_TRANSMIT, 0x00 );
 		}
 
 		/* Detect Falling Edge of GPIO */
@@ -99,43 +94,44 @@ int32 _user_start() {
 			_gpiotoggle( GPIO_BUSY_TOGGLE, _GPIOTOGGLE_SWAP ); // Busy Toggle
 			detect_parallel &= 0x1F; // 0-31
 			if ( detect_parallel & 0b10000 ) { // Command
-				number_command = detect_parallel & 0b1111;
-				target_address_position = 0;
-				buffer_data_upper = false;
-				if ( number_command == 4 ) { // Immediate Swap FRONT/BACK
-					front_address_swap = DMX32_BUFFER_FRONT;
-					back_address_swap = DMX32_BUFFER_BACK;
+				if ( detect_parallel == 20 ) { // Immediate Swap FRONT/BACK
+					front_buffer_swap = DMX32_BUFFER_FRONT;
+					back_buffer_swap = DMX32_BUFFER_BACK;
 					arm32_dsb();
-					_store_32( (uint32)&DMX32_BUFFER_FRONT, back_address_swap );
-					_store_32( (uint32)&DMX32_BUFFER_BACK, front_address_swap );
-				} else if ( number_command == 5 ) { // Send FRONT
-					_store_32( (uint32)&OS_IRQ_SWAP, 0x00 );
-				} else if ( number_command == 6 ) { // Send FRONT and Swap FRONT/BACK on End of Packet
-					_store_32( (uint32)&OS_IRQ_SWAP, 0x01 );
+					_store_32( (uint32)&DMX32_BUFFER_FRONT, back_buffer_swap );
+					_store_32( (uint32)&DMX32_BUFFER_BACK, front_buffer_swap );
+				} else if ( detect_parallel == 21 ) { // Send FRONT
+					_store_32( (uint32)&OS_FIQ_SWAP, 0x00 );
+				} else if ( detect_parallel == 22 ) { // Send FRONT and Swap FRONT/BACK on End of Packet
+					_store_32( (uint32)&OS_FIQ_SWAP, 0x01 );
+				} else if ( detect_parallel != 16 ) { // Select Data Mode
+					data_mode = detect_parallel & 0b1111;
 				}
-			} else { // Address or Data
-				if ( number_command == 1 ) { // Address
-					if ( target_address_position == 0 ) {
-						target_address = (target_address & 0x1F0) | (detect_parallel & 0b1111);
-						target_address_position = 1;
-					} else if ( target_address_position == 1 ) {
-						target_address = (target_address & 0x10F) | ((detect_parallel & 0b1111) << 4 );
-						target_address_position = 2;
-					} else if ( target_address_position == 2 ) {
-						target_address = (target_address & 0x0FF) | ((detect_parallel & 0b1) << 8 );
-						target_address_position = 0;
+				index_slot_position = 0;
+				buffer_data_upper_flag = false;
+			} else { // Data
+				if ( data_mode == 1 ) { // Slot Index
+					if ( index_slot_position == 0 ) {
+						index_slot = (index_slot & 0x1F0) | (detect_parallel & 0b1111);
+						index_slot_position = 1;
+					} else if ( index_slot_position == 1 ) {
+						index_slot = (index_slot & 0x10F) | ((detect_parallel & 0b1111) << 4 );
+						index_slot_position = 2;
+					} else if ( index_slot_position == 2 ) {
+						index_slot = (index_slot & 0x0FF) | ((detect_parallel & 0b1) << 8 );
+						index_slot_position = 0;
 					}
-				} else if ( number_command > 1 ) { // Data
-					if ( ! buffer_data_upper ) {
+				} else if ( data_mode > 1 ) { // Value
+					if ( ! buffer_data_upper_flag ) {
 						buffer_data = (buffer_data & 0xF0) | (detect_parallel & 0b1111);
-						buffer_data_upper = true;
+						buffer_data_upper_flag = true;
 					} else {
 						buffer_data = (buffer_data & 0x0F) | ((detect_parallel & 0b1111) << 4 );
-						buffer_data_upper = false;
-						_store_8( DMX32_BUFFER_BACK + target_address + 1, buffer_data );
-						if ( number_command == 3 ) { // Sequencial
-							target_address++;
-							if ( target_address >= DMX512_LENGTH ) target_address = 0;
+						buffer_data_upper_flag = false;
+						_store_8( DMX32_BUFFER_BACK + index_slot, buffer_data );
+						if ( data_mode == 3 ) { // Sequencial
+							index_slot++;
+							if ( index_slot >= DMX512_LENGTH ) index_slot = 0;
 						}
 					}
 				}
